3be0600b56051a454be2c38c816b390404cf35f9
MarkTinega <martinozas@gmail.com>
2023-08-12 19:28:35 +0300

    Fixed the async.gather bug on task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,6 +2,8 @@
 """A Module that measure the Runtime for four parallel comprehensions"""
 
 import asyncio
+import time
+
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -15,15 +17,10 @@ async def measure_runtime() -> float:
         float: Total runtime of executing async_comprehension
         4 times in parallel.
     """
-    start_time = asyncio.get_event_loop().time()
+    start_time = time.time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
 
-    end_time = asyncio.get_event_loop().time()
+    end_time = time.time()
     total_runtime = end_time - start_time
     return total_runtime
