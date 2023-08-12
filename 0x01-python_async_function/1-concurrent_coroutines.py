776f64acac65ce21ad58c3c9354ae7d1aa04a5ea
MarkTinega <martinozas@gmail.com>
2023-08-12 17:19:44 +0300

    Fixed python styling

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,4 +15,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that executes multiple coroutines with async"""
     coroutines = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*coroutines) 
+    return await asyncio.gather(*coroutines)
