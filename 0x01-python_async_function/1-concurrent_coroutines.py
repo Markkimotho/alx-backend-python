06f2cc4e225964b754798865a849be900cc7b5d4
MarkTinega <martinozas@gmail.com>
2023-08-12 17:16:49 +0300

    Fixed the ordering without using sort

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,6 +14,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that executes multiple coroutines with async"""
-    delays = await asyncio.gather(*[wait_random(max_delay)
-                                    for _ in repeat(None, n)])
-    return sorted(delays, key=lambda x: randint(0, max_delay))
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*coroutines) 
