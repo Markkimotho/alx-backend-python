f1db2c175f38a99a9d411425eb36b0595c176ffe
MarkTinega <martinozas@gmail.com>
2023-08-12 19:23:11 +0300

    used the gather method on 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,14 +2,18 @@
 """A Module that measure the Runtime for four parallel comprehensions"""
 
 import asyncio
-import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Coroutine that runs async_comprehension 4 times in parallel
-        using asyncio.gather and measures its runtime
+    """
+    Coroutine that runs async_comprehension 4 times in parallel
+    using asyncio.gather and measures its runtime.
+
+    Returns:
+        float: Total runtime of executing async_comprehension
+        4 times in parallel.
     """
     start_time = asyncio.get_event_loop().time()
 
@@ -19,6 +23,7 @@ async def measure_runtime() -> float:
         async_comprehension(),
         async_comprehension()
     )
+
     end_time = asyncio.get_event_loop().time()
     total_runtime = end_time - start_time
     return total_runtime
