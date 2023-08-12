1047f0436ec35a44165ef8d12c9ce753ce6b1d2a
MarkTinega <martinozas@gmail.com>
2023-08-12 17:24:54 +0300

    Fixed the ordering and styling

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,15 +4,25 @@ at the same time"""
 
 import asyncio
 from typing import List
-from random import randint
-from itertools import repeat
 from functools import partial
+from itertools import repeat
+from random import randint
+from time import sleep
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Function that executes multiple coroutines with async"""
-    coroutines = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*coroutines)
+    """Coroutine that spawns wait_random n times
+    with the specified max_delay
+    """
+    delays = []
+
+    async def process_delay():
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    await asyncio.gather(*[process_delay() for _ in range(n)])
+
+    return sorted(delays)
