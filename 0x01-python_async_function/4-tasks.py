a1cea2cc98de177bca99e42e55cc970ff3ebac6b
MarkTinega <martinozas@gmail.com>
2023-08-12 17:40:08 +0300

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Module that shows how async python works"""
+
 import asyncio
-from typing import List, Any
-from random import randint
-from itertools import repeat
+from typing import List
 from functools import partial
 
 
@@ -11,12 +10,19 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function similar to that in task 3
-    but task_wait_random is being called
+    """Coroutine that spawns new tasks with task_wait_random
+        n times with the specified max_delay
     """
-    tasks = [task_wait_random(max_delay) for _ in repeat(None, n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays, key=lambda x: randint(0, max_delay))
+    tasks = []
+
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+    delays = [task.result() for task in tasks]
+    return delays
 
 
 if __name__ == '__main__':
